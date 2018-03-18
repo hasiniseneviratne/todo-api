00b8ee9cea3bdd76ee765d00980868199bea9cd0
Hasini_Seneviratne <hasiniseneviratne94@gmai.com>
2018-03-18 16:45:38 +0530

    Get todo by id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {ObjectId} = require('mongodb');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
 
@@ -26,6 +27,24 @@ app.get('/todos', (req, res) => {
    }, (e) => {
        res.status(400).send(e);
    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (! ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send({todo});
+        } else {
+            return res.status(404).send();
+        }
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(3000, ()=> {
