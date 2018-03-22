404fdf838a9f56196a32708de7c1cad1ba1c10a7
Hasini_Seneviratne <hasiniseneviratne94@gmai.com>
2018-03-22 23:28:12 +0530

    setup db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose =require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp' || 'mongodb://Admin:admin@ds143744.mlab.com:43744/todo-api');
+mongoose.connect('mongodb://admin:admin@ds143744.mlab.com:43744/todo-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
