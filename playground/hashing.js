fb325b26f33c261a3f28481c537ae9a5421a0274
Hasini_Seneviratne <hasiniseneviratne94@gmai.com>
2018-04-09 20:57:01 +0530

    Hashing passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var data = {
     id: 10
@@ -34,3 +35,17 @@ console.log(decoded);
 //} else {
 //    console.log('Data does not match');
 //}
+
+var password = 'abc123!';
+
+//bcrypt.genSalt(10, (err, salt) => {
+//    bcrypt.hash(password, salt, (err, hash) => {
+//        console.log(hash);
+//    });
+//});
+
+var hashedPassword = '$2a$10$juNe3JV1oSg.LEvEtyNj3eItGVnFcg9Qc2yAf.YOaDIlunTDRnD8a';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
