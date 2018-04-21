a6c16675de93bbdf95cc64f5e231854750baeb62
Hasini_Seneviratne <hasiniseneviratne94@gmai.com>
2018-04-22 00:49:09 +0530

    Updated config file and added json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,18 @@
 var env = process.env.NODE_ENV || 'development';
 
-process.env.MONGODB_URI = 'mongodb://admin:admin@ds143744.mlab.com:43744/todo-api'
+if (env === 'development' || env === "test") {
+    var config = require('./config.json');
+    var envConfig = config[env];
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    Object.keys(envConfig).forEach((key) => {
+       process.env[key] = envConfig[key];
+    });
 }
+
+//if (env === 'development') {
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+//} else if (env === 'test') {
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+//}
